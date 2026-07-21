311c71b799f08ffb60b330fc3234612439222d7f
@@tifa Rizvi <rizviaatifa235@gmail.com>
2026-07-21 08:42:31 +0530

    update url

--- backend/main.py
+++ backend/main.py
@@ -17,9 +17,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://callsense-ai-aatifa-rizvis-projects.vercel.app",
-        "http://localhost:5173"
+        "https://callsense-ai-pi.vercel.app",
+        "http://localhost:3000",
     ],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
